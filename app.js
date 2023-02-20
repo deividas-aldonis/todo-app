f7b873521035c0368416c7dd9ca467ce4c7cf465
Deividas <deividas.aldonis.dev@gmail.com>
2023-02-21 01:20:32 +0200

    refactor js logic

--- app.js
+++ app.js
@@ -10,11 +10,133 @@ const todoDndTip = document.querySelector(".dnd-tip");
 
 const todoItemsLeft = document.querySelector(".todo-left");
 const todoList = document.querySelector(".todo-list");
-const addBtn = document.querySelector(".add-icon");
+const createTodoBtn = document.querySelector(".add-icon");
 const addInput = document.querySelector(".add-todo-input");
 
-let todoLeftItemCounter = 0;
-const todos = [];
+const clearBtn = document.querySelector(".clear-btn");
+
+let todos = [];
+let filter = "all";
+
+createTodoBtn.addEventListener("click", createTodo);
+clearBtn.addEventListener("click", clearCompleted);
+todoFilter.addEventListener("click", filterTodos);
+
+function createTodo() {
+  const temp = document.getElementsByTagName("template")[0];
+  const clone = temp.content.cloneNode(true);
+
+  const li = document.createElement("li");
+  if (filter === "completed") li.classList.add("hide");
+  li.classList.add("todo-item");
+  li.id = uuidv4();
+  li.appendChild(clone);
+
+  const paraText = addInput.value.trim();
+  const para = li.querySelector("p");
+  para.textContent = paraText;
+
+  const removeBtn = li.querySelector(".delete-btn");
+  removeBtn.addEventListener("click", removeTodo);
+
+  const completeBtn = li.querySelector(".complete-todo-btn");
+  completeBtn.addEventListener("click", completeTodo);
+
+  todos.push(li);
+
+  renderTodos();
+  updateLeftItemCounter();
+}
+
+function renderTodos() {
+  if (todos.length === 0) {
+    todoContainer.classList.add("hide");
+    todoFilter.classList.add("hide");
+    todoDndTip.classList.add("hide");
+  } else {
+    todoContainer.classList.remove("hide");
+    todoFilter.classList.remove("hide");
+    todoDndTip.classList.remove("hide");
+  }
+
+  todoList.replaceChildren(...todos);
+}
+
+function removeTodo(e) {
+  const todoItemID = e.target.closest(".todo-item").id;
+
+  const todoItemIndex = todos.findIndex((t) => t.id === todoItemID);
+  todos.splice(todoItemIndex, 1);
+
+  renderTodos();
+  updateLeftItemCounter();
+}
+
+function completeTodo(e) {
+  const todoItem = e.target.closest(".todo-item");
+
+  if (todoItem.classList.contains("done")) {
+    todoItem.classList.remove("done");
+
+    if (filter === "completed") {
+      todoItem.classList.add("hide");
+    }
+  } else {
+    todoItem.classList.add("done");
+
+    if (filter === "active") {
+      todoItem.classList.add("hide");
+    }
+  }
+
+  updateLeftItemCounter();
+}
+
+function updateLeftItemCounter() {
+  const activeTodosLength = todos.filter(
+    (t) => !t.classList.contains("done")
+  ).length;
+  let itemLeftText;
+
+  if (activeTodosLength === 1) itemLeftText = "1 item left";
+  else itemLeftText = `${activeTodosLength} items left`;
+
+  todoItemsLeft.textContent = itemLeftText;
+}
+
+function clearCompleted() {
+  todos = todos.filter((t) => !t.classList.contains("done"));
+
+  renderTodos();
+  updateLeftItemCounter();
+}
+
+function filterTodos(e) {
+  const clicked = e.target.tagName.toLowerCase();
+
+  if (!clicked === "button") return;
+
+  [...e.target.parentElement.children].forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  e.target.classList.add("active");
+
+  filter = e.target.dataset.filter;
+  const todoItems = [...todoList.children];
+  todoItems.forEach((todo) => todo.classList.remove("hide"));
+
+  if (filter === "active") {
+    const completedTodos = todoItems.filter((todo) =>
+      todo.classList.contains("done")
+    );
+    completedTodos.forEach((todo) => todo.classList.add("hide"));
+  } else if (filter === "completed") {
+    const activeTodos = todoItems.filter(
+      (todo) => !todo.classList.contains("done")
+    );
+    activeTodos.forEach((todo) => todo.classList.add("hide"));
+  }
+}
 
 // Theme switcher logic
 themeSwitchBtn.addEventListener("click", (e) => {
@@ -31,132 +153,5 @@ themeSwitchBtn.addEventListener("click", (e) => {
     target.setAttribute("data-current-theme", "light");
     headerBackgroundImage.style.backgroundImage =
       'url("./images/bg-mobile-light.jpg")';
-  }
-});
-
-// CRUD logic
-addBtn.addEventListener("click", () => {
-  const inputValue = addInput.value.trim();
-
-  if (!inputValue) return;
-
-  const id = uuidv4();
-
-  const listItem = `<li class="todo-item" id=${id} >
-                      <div>
-                        <div class="icon complete-todo-btn prevent-select">
-                          <img
-                            class="icon-image"
-                            src="./images/icon-check.svg"
-                            alt="check icon"
-                          />
-                        </div>
-                        <p>${inputValue}</p>
-                      </div>
-                      <svg class="delete-btn" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" />
-                      </svg>
-                    </li>`;
-
-  todoList.insertAdjacentHTML("beforeend", listItem);
-
-  const todoItems = todoList.querySelectorAll(".todo-item");
-  const newlyAddedTodo = todoItems[todoItems.length - 1];
-
-  const todoAddBtn = newlyAddedTodo.querySelector(".complete-todo-btn");
-  const todoDeleteBtn = newlyAddedTodo.querySelector(".delete-btn");
-
-  todoAddBtn.addEventListener("click", completeTodo);
-  todoDeleteBtn.addEventListener("click", deleteTodo);
-
-  updateTodoUI();
-  addTodoLeft();
-  addInput.value = "";
-  todos.push(newlyAddedTodo);
-});
-
-function completeTodo(e) {
-  const todoItem = e.target.closest(".todo-item");
-
-  if (todoItem) {
-    if (todoItem.classList.contains("done")) {
-      todoItem.classList.remove("done");
-      addTodoLeft();
-    } else {
-      todoItem.classList.add("done");
-      removeTodoLeft();
-    }
-  }
-}
-
-function deleteTodo(e) {
-  const todoItem = e.target.closest(".todo-item");
-  todoItem.remove();
-
-  const index = todos.findIndex((t) => t.id === todoItem.id);
-  todos.splice(index, 1);
-
-  updateTodoUI();
-
-  if (!todoItem.classList.contains("done")) {
-    removeTodoLeft();
-  }
-}
-
-function updateTodoUI() {
-  const todoItems = document.querySelectorAll(".todo-item").length;
-
-  if (todoItems === 0) {
-    todoContainer.classList.add("hide");
-    todoDndTip.classList.add("hide");
-    todoFilter.classList.add("hide");
-  } else if (todoItems > 0 && todoContainer.classList.contains("hide")) {
-    todoContainer.classList.remove("hide");
-    todoDndTip.classList.remove("hide");
-    todoFilter.classList.remove("hide");
-  }
-}
-
-function addTodoLeft() {
-  todoLeftItemCounter++;
-
-  if (todoLeftItemCounter === 1) {
-    todoItemsLeft.textContent = "1 item left";
-  } else {
-    todoItemsLeft.textContent = `${todoLeftItemCounter} items left`;
-  }
-}
-
-function removeTodoLeft() {
-  todoLeftItemCounter--;
-
-  if (todoLeftItemCounter === 1) {
-    todoItemsLeft.textContent = "1 item left";
-  } else {
-    todoItemsLeft.textContent = `${todoLeftItemCounter} items left`;
-  }
-}
-
-todoFilter.addEventListener("click", (e) => {
-  const target = e.target.tagName.toLowerCase();
-
-  todoFilter.querySelectorAll("button").forEach((btn) => {
-    btn.classList.remove("active");
-  });
-
-  if (target === "button") {
-    e.target.classList.add("active");
-
-    const filter = e.target.dataset.filter;
-
-    if (filter === "completed") {
-      const completed = todos.filter((t) => t.classList.contains("done"));
-      todoList.replaceChildren(...completed);
-    } else if (filter === "active") {
-      const active = todos.filter((t) => !t.classList.contains("done"));
-      todoList.replaceChildren(...active);
-    } else {
-      todoList.replaceChildren(...todos);
-    }
   }
 });
